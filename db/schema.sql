55ef94f1e1bda330303511370ca9ebe397008d92
Eli Cadena <103008210+ecadena7@users.noreply.github.com>
2023-03-19 18:25:38 -0600

    cleaning up code in the schema.sql file

--- db/schema.sql
+++ db/schema.sql
@@ -6,14 +6,14 @@ USE company_db;
 
 CREATE TABLE department (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name: VARCHAR(30) NOT NULL
+    name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE role (
-    id: INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    title: VARCHAR(30) NOT NULL,
-    salary: DECIMAL(7, 2),
-    department_id: INT,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(7, 2),
+    department_id INT,
     INDEX dep_ind (department_id),
     CONSTRAINT fk_department 
     FOREIGN KEY (department_id) 
@@ -22,16 +22,16 @@ CREATE TABLE role (
 )
 
 CREATE TABLE employee (
-    id: INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    first_name: VARCHAR(75) NOT NULL,
-    last_name: VARCHAR(75) NOT NULL,
-    role_id: INT,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(75) NOT NULL,
+    last_name VARCHAR(75) NOT NULL,
+    role_id INT,
     INDEX rol_ind (role_id),
     CONSTRAINT fk_role
     FOREIGN KEY (role_id)
     REFERENCES role(id) -- hold reference to employee role
     ON DELETE CASCADE, -- deleting both parent & child tables
-    manager_id: INT,
+    manager_id INT,
     INDEX man_ind (manager_id), 
     CONSTRAINT fk_manager 
     FOREIGN KEY (manager_id) -- that is the manager of current employee
--- db/seeds.sql
+++ db/seeds.sql
@@ -15,11 +15,5 @@ VALUES ("Manager", 1, 1000000 ),
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES ("Mae", "The Corgi", 1, NULL),
-       ("Data", "Star Treck", 2, NULL),
+       ("Data", "Star Trek", 2, NULL),
        ("Lando", "Starwars", 3, NULL);
-
-review form - complete this!!!!
-https://docs.google.com/forms/d/e/1FAIpQLSdb4ejjbqoqKO-Q4k7zeO_xwykwB0dxYLWYm1mX5Ik45MzEeg/viewform
-
-
-https://calendly.com/fsf-tutor-team/sean-brown-byfield
